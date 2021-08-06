5c233ab6e61c510dd8829a3702bd03ff13c82663
Saqib <saqi_mm@hotmail.com>
2021-08-07 00:34:24 +0500

    first commit

---
+++ wTheme/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function load_stylesheets(){
+    wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', '', 1, 'all');
+    wp_enqueue_style('stylesheet');
+    
+    wp_register_style('custom', get_template_directory_uri() . '/app.css', '', 1, 'all');
+    wp_enqueue_style('custom');
+}
+add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+function load_javascript(){
+    wp_register_script('custom', get_template_directory_uri() . '/app.js', 'jquery', 1, true);
+    wp_enqueue_script('custom');
+}
+add_action('wp_enqueue_scripts', 'load_javascript');
